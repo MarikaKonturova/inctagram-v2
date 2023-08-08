5be38fbc1d13e9dac0d015e4cfd6708fa802fea3
Margarita P <rprivalko@gmail.com>
2023-08-08 16:20:55 +0300

    fix: update device

--- src/templates/profile-settings/DeviceList/model/useDevices.ts
+++ src/templates/profile-settings/DeviceList/model/useDevices.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { type AxiosResponse } from 'axios'
 import { deviceService } from 'shared/api/devices/deviceService'
-import { type DeviceScheme } from 'shared/types/device'
+import { type AllDevicesScheme } from 'shared/types/device'
 
 export const useDevices = () => {
-    return useQuery<AxiosResponse<DeviceScheme[]>>(['devices'], {
+    return useQuery<AxiosResponse<AllDevicesScheme>>(['devices'], {
         queryFn: deviceService.getDevices,
         retry: false
     })
