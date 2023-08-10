0e9184cc523f1b60a40ab8fb98801588930f7a5c
Margarita P <rprivalko@gmail.com>
2023-08-10 10:59:57 +0300

    fix: fix eslint errors

--- src/shared/types/device.ts
+++ src/shared/types/device.ts
@@ -1,11 +1,11 @@
 export interface DeviceScheme {
-  ip: string;
-  userAgent: string;
-  lastVisit: string;
-  deviceId: string;
+    ip: string
+    userAgent: string
+    lastVisit: string
+    deviceId: string
 }
 
 export interface AllDevicesScheme {
-  devices: DeviceScheme[];
-  currentDeviceId: string;
+    devices: DeviceScheme[]
+    currentDeviceId: string
 }
