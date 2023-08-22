68c1edcd79d5949beabbe6f9f9d53407b3f218c3
Vladyslavua <vladgershman@gmail.com>
2023-08-22 13:43:21 +0200

    init:made a Google authorization and registration component

---
+++ src/features/auth/ui/GoogleAuth/model/index.ts
@@ -0,0 +1,26 @@
+import {useQuery} from "@tanstack/react-query";
+
+interface useGoogleAuthProps {
+    type:'Registration' | 'Login'
+}
+
+export const GOOGLE_AUTHORIZATION = {
+    registrationGoogle(){
+        window.location.assign(`https://twin.cygan.lol/auth/google/registration`)
+    },
+    loginGoogle() {
+        window.location.assign(`https://twin.cygan.lol/auth/google/authorization`)
+    }
+}
+
+
+export const useGoogleAuth = ({type}:useGoogleAuthProps) => {
+
+    const { refetch } = useQuery({
+        queryKey:['googleAuth'],
+        queryFn:type === 'Registration' ? GOOGLE_AUTHORIZATION.registrationGoogle : GOOGLE_AUTHORIZATION.loginGoogle,
+        enabled:false,
+    })
+
+    return { refetch }
+}
