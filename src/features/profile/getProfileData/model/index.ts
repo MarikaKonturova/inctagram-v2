98c9b9b1233e9d71c27d5c66495edc61e698e5e5
Vladyslavua <vladgershman@gmail.com>
2023-09-11 18:21:53 +0200

    update: Added 'setAuth' state to Google,GitHub and 'getProfileData' components

--- src/features/profile/getProfileData/model/index.ts
+++ src/features/profile/getProfileData/model/index.ts
@@ -2,13 +2,16 @@ import { useQuery } from '@tanstack/react-query'
 import { type AxiosError } from 'axios'
 import { useSnackbar } from 'widgets/SnackBar/model/store/snackbarStore'
 import { profileService } from 'shared/api/profile'
+import { useAuth } from '../../../auth'
 
 export const useGetProfileData = () => {
+    const { setAuth } = useAuth()
     const onOpen = useSnackbar((state) => state.onOpen)
     const { data: response, isLoading } = useQuery(['getProfileData'],
         () => profileService.getProfileData(), {
             onSuccess: ({ data }) => {
                 console.log(data)
+                setAuth(true)
             },
             onError: (error: AxiosError<{ message: string }>) => {
                 onOpen(error.message, 'danger', 'left')
