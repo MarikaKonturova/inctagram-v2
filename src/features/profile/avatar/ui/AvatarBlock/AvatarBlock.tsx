cf688e6743105a6b4772ad1553e168a24baf3688
Margarita P <rprivalko@gmail.com>
2023-10-20 11:30:29 +0300

    fix: rename branch

--- src/features/profile/avatar/ui/AvatarBlock/AvatarBlock.tsx
+++ src/features/profile/avatar/ui/AvatarBlock/AvatarBlock.tsx
@@ -25,7 +25,6 @@ export const AvatarBlock = ({ avatars }: any) => {
         }
     }, [avatars])
 
-    console.log(avatars)
     return (
         <div className={cls.avatarContainer}>
             <AvatarModal setAvatar={setAvatar} />
