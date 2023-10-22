fb1fdf310bafe3a013fe0c386fad9e31e41ce4d4
Margarita P <rprivalko@gmail.com>
2023-10-22 16:00:39 +0300

    fix: refactor General Information

--- src/templates/profile-settings/GeneralInformation/index.tsx
+++ src/templates/profile-settings/GeneralInformation/index.tsx
@@ -1,9 +1,14 @@
 import { GeneralInformationForm } from 'features/general-information-form'
+import { AvatarBlock } from 'features/profile/avatar/ui/AvatarBlock/AvatarBlock'
 import { useGetProfileData } from 'features/profile/getProfileData/model'
+import cls from './styles.module.scss'
 
 export default function GeneralInformation () {
     const { response } = useGetProfileData()
     const userData = response?.data
 
-    return <GeneralInformationForm userData={userData} />
+    return <div className={cls.container}>
+        <AvatarBlock avatars={userData?.avatars} />
+        <GeneralInformationForm userData={userData} />
+    </div>
 }
