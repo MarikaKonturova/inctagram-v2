225e88779704d6cb8b1678df421a2d7bb7c956d1
Vladyslavua <vladgershman@gmail.com>
2023-12-05 17:50:41 +0100

    Fix:Fixed hydration issue at login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,20 @@
 import { getAuthLayout } from 'layouts/Layout/AuthLayout/AuthLayout'
 import { type GetServerSidePropsContext } from 'next'
+import { useEffect, useState } from 'react'
 import { ConfirmModal, LoginForm } from 'features/auth'
 import { getTranslations } from 'shared/lib/i18n'
 
 export default function Login () {
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    if (!isMounted) {
+        return null
+    }
+
     return <>
         <LoginForm/>
         <ConfirmModal/>
