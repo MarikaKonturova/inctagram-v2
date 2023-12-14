8c3838eab1e6e73648df857902458890df8f94dd
Vladyslavua <vladgershman@gmail.com>
2023-12-14 15:11:51 +0100

    Fix: Fixed issue with refetch data

--- src/features/post/getCommentaries/model/index.ts
+++ src/features/post/getCommentaries/model/index.ts
@@ -1,13 +1,17 @@
 import { useQuery } from '@tanstack/react-query'
 import { type AxiosError } from 'axios'
 import { useSnackbar } from 'features/common'
+import { useCommentStore } from 'features/profile/getPosts/model'
 import { MyPostService, PostService } from 'shared/api'
 
 export const useGetPostComments = (postId: number) => {
     const onOpen = useSnackbar((state) => state.onOpen)
+    const { refetch, setRefetch } = useCommentStore()
     const { isLoading, error, data } = useQuery({
-        queryKey: ['postComments', 'postAnswers'],
+        queryKey: ['postComments', 'postComments', postId],
         queryFn: () => MyPostService.getPostComments(postId),
+        onSuccess: () => { setRefetch({ doRefetch: false }) },
+        refetchInterval: refetch.doRefetch ? 100 : false,
         onError: (error: AxiosError<{ message: string }>) => {
             onOpen(error.message, 'danger', 'left')
         },
@@ -23,7 +27,7 @@ export const useGetPostAnswersForComments = (postId: number, commentId: number) 
     const { data } = useQuery({
         queryKey: ['postAnswers', 'postComments', commentId],
         queryFn: () => PostService.getAnswerForComment(postId, commentId),
-        enabled: commentId != null
+        enabled: !!commentId
     })
 
     const answerForComment = data?.data
