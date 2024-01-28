cdfb49f776c066077e0329a20292678fc16afc6f
MrUnderSan <MrUnderSan@gmail.com>
2024-01-29 00:55:18 +0300

    SCRUM-209 refactor: shorten imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
+import { useAuth } from 'features/auth'
+import { SelectIsAuth } from 'features/auth/model/selectors'
 import { getLayoutWithoutSidebar } from 'layouts/Layout/LayoutWithoutSideBar/LayoutWithoutSidebar'
 import { type GetServerSidePropsContext } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { AppRoutes } from 'shared/constants/path'
 import { getTranslations } from 'shared/lib/i18n'
-
-import { useAuth } from '../features/auth'
-import { SelectIsAuth } from '../features/auth/model/selectors'
-import { AppRoutes } from '../shared/constants/path'
 
 export default function Home() {
   const isAuth = useAuth(SelectIsAuth)
