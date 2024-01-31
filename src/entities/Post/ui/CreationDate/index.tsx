47c4a6cb38236bf28ef09e7a85c50ea9cf4d873e
MrUnderSan <MrUnderSan@gmail.com>
2024-01-31 17:43:34 +0300

    SCRUM-209 feat: add className to Props

--- src/entities/Post/ui/CreationDate/index.tsx
+++ src/entities/Post/ui/CreationDate/index.tsx
@@ -1,9 +1,15 @@
+import clsx from 'clsx'
 import React, { type FC } from 'react'
 
 import cls from './styles.module.scss'
 
 interface Props {
+  className?: string
   date: string
 }
 
-export const CreationDate: FC<Props> = ({ date }) => <div className={cls.container}>{date}</div>
+export const CreationDate: FC<Props> = ({ className, date }) => {
+  const classNames = clsx(cls.container, className)
+
+  return <div className={classNames}>{date}</div>
+}
