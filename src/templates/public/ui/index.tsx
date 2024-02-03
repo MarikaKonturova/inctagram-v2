6ce7e32aaca3c285aa5dd059a592291706fe0c95
MrUnderSan <MrUnderSan@gmail.com>
2024-02-03 10:45:56 +0300

    SCRUM-209 fix: replace CSR with SSG

--- src/templates/public/ui/index.tsx
+++ src/templates/public/ui/index.tsx
@@ -1,14 +1,14 @@
-import {useQuery} from '@tanstack/react-query';
 import {useState} from 'react';
-import {HomeService} from 'shared/api/home';
-import {ResponseItem} from 'shared/types/home';
+import {LastPublicationsResponse, ResponseItem} from 'shared/types/home';
 import {PublicPostList, PublicPostModal, RegisteredUsers} from 'widgets/Public';
 
 import cls from './styles.module.scss'
 
-export const PublicPage = () => {
+export interface PublicPageProps {
+  data: LastPublicationsResponse
+}
 
-const {data} = useQuery({queryFn: HomeService.getLastPublications})
+export const PublicPage = ({data}: PublicPageProps) => {
 
   const [modalPost, setModalPost] = useState<ResponseItem | null>(null)
 
@@ -22,12 +22,11 @@ const {data} = useQuery({queryFn: HomeService.getLastPublications})
 
   return (
     <div className={cls.container}>
-      <RegisteredUsers className={cls.users} count={data?.data?.countUsers} />
-      {data?.data?.lastPosts && <PublicPostList openModal={openModal} posts={data.data.lastPosts} />}
+      <RegisteredUsers className={cls.users} count={data.countUsers} />
+      {data.lastPosts && <PublicPostList openModal={openModal} posts={data.lastPosts} />}
       {modalPost &&
           <PublicPostModal handleClose={closeModal} isOpen={!!modalPost} post={modalPost} />
       }
-
     </div>
   )
 }
