f812de4847a849542fa049c65da57728a57212fa
Ruban Pavel <ruban.pavel@outlook.com>
2024-02-15 02:45:55 +0300

    SCRUM-249 fix: correct display of like counter in comments

--- src/features/post/likeComment/ui/LikeCommentIconButton/LikeCommentIconButton.tsx
+++ src/features/post/likeComment/ui/LikeCommentIconButton/LikeCommentIconButton.tsx
@@ -14,7 +14,7 @@ interface likeCommentType {
 }
 
 export const LikeCommentIconButton: FC<likeCommentType> = ({ commentId, isLiked, postId }) => {
-  const { likeComment } = useLikeComment(postId, commentId)
+  const { isLoading, likeComment } = useLikeComment(postId, commentId)
   const onLikeIconClick = () => {
     likeComment({ likeStatus: isLiked ? LikeStatus.DISLIKE : LikeStatus.LIKE })
   }
@@ -24,6 +24,7 @@ export const LikeCommentIconButton: FC<likeCommentType> = ({ commentId, isLiked,
       className={cls.icon}
       filledIcon={<IconLike className={cls.likeIcon} fill={'#CC1439'} />}
       initialState={isLiked}
+      isLoading={isLoading}
       onClick={onLikeIconClick}
       outlineIcon={<IconLikeOutline className={cls.likeIcon} fill={'#ffffff'} />}
     />
