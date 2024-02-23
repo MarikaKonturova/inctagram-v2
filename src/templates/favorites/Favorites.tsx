98c9aca0f7379fcb42ad51cb0026f286f588b13e
Ruban Pavel <ruban.public@gmail.com>
2024-02-23 16:27:36 +0300

    SCRUM-294 fix: add logic post switching buttons in Favorites

--- src/templates/favorites/Favorites.tsx
+++ src/templates/favorites/Favorites.tsx
@@ -1,9 +1,10 @@
-import { InfiniteData } from '@tanstack/react-query'
+import { InfiniteData, useQueryClient } from '@tanstack/react-query'
 import { useGetFavoritesData } from 'entities/Favorites'
 import { useGetMyPost } from 'entities/Post'
 import { useGetProfileData } from 'entities/Profile'
 import React, { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
+import { MyPostService } from 'shared/api'
 import { MODALS, type Values } from 'shared/constants/post'
 import { type FavoritesType } from 'shared/types/post'
 import { Card, Loader } from 'shared/ui'
@@ -13,6 +14,7 @@ import { GetPostModal, PostModalActions } from 'widgets/Post'
 import cls from './Favorites.module.scss'
 
 export const FavoritesPage = () => {
+  const queryClient = useQueryClient()
   const { response } = useGetProfileData()
   const userData = response?.data
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess } = useGetFavoritesData(
@@ -20,8 +22,48 @@ export const FavoritesPage = () => {
   )
   const { inView, ref } = useInView({ threshold: 0.0 })
   const [currentModal, setCurrentModal] = useState<Values | null>(null)
-  const [postId, setPostId] = useState<number | undefined>(undefined)
-  const { post } = useGetMyPost(postId || 0)
+  const [postId, setPostId] = useState<number>(0)
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [currentPost, setCurrentCurrentPost] = useState<number>(0)
+  const { post } = useGetMyPost(currentPost)
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const idsArray: number[] =
+    data && data.pages
+      ? data.pages.flatMap(page => page.items.map((item: { id: number }) => item.id))
+      : []
+
+  const findIndex = idsArray.findIndex(id => id === postId)
+  const firstElement = idsArray[0] === idsArray[currentIndex]
+  const lastElement = idsArray[idsArray.length - 1] === idsArray[currentIndex]
+
+  useEffect(() => {
+    if (idsArray[currentIndex]) {
+      setCurrentCurrentPost(idsArray[currentIndex])
+    } else {
+      setCurrentCurrentPost(postId)
+    }
+  }, [postId, idsArray, currentIndex])
+
+  useEffect(() => {
+    setCurrentIndex(findIndex)
+  }, [postId])
+
+  useEffect(() => {
+    {
+      idsArray.map(id => {
+        queryClient.prefetchQuery(['post', id], () => MyPostService.getPost(id))
+      })
+    }
+  }, [idsArray])
+
+  const handleClick = (direction: 'back' | 'next') => {
+    if (direction === 'back' && currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1)
+    } else if (direction === 'next' && currentIndex < idsArray.length - 1) {
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
 
   useEffect(() => {
     if (inView && hasNextPage) {
@@ -38,6 +80,7 @@ export const FavoritesPage = () => {
 
   const closeModal = () => {
     setCurrentModal(null)
+    setCurrentIndex(0)
   }
   const renderContent = (page: FavoritesType) => {
     return page.items.map(item => {
@@ -64,14 +107,17 @@ export const FavoritesPage = () => {
         {(data as InfiniteData<FavoritesType>)?.pages.map(page => page && renderContent(page))}
       </div>
 
-      {postId && post && userData && (
+      {!!postId && !!post && !!userData && (
         <GetPostModal
           actionsSlot={<PostModalActions post={post} />}
-          content={<Commentaries key={postId} postId={postId} />}
+          content={<Commentaries key={postId} postId={idsArray[currentIndex] || postId} />}
+          firstElement={firstElement}
+          handleClick={handleClick}
           handleClose={closeModal}
           id={MODALS.GetPostModal}
           isOpen={currentModal === MODALS.GetPostModal}
           key={postId}
+          lastElement={lastElement}
           post={post}
           userName={response?.data.userName}
         />
