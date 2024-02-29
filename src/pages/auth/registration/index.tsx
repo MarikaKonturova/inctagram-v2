db70d01448f63573f498d0e1c5fae2d852a7dbcc
Margarita P <rprivalko@gmail.com>
2024-02-29 12:49:20 +0300

    SCRUM-297 fix: application error when changing the language and reloading the page

--- src/pages/auth/registration/index.tsx
+++ src/pages/auth/registration/index.tsx
@@ -1,9 +1,20 @@
 import { ConfirmModal, RegisterForm } from 'features/auth'
 import { getAuthLayout } from 'layouts'
 import { type GetServerSidePropsContext } from 'next'
+import { useEffect, useState } from 'react'
 import { getTranslations } from 'shared/lib/i18n'
 
 export default function Registration() {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
+
   return (
     <>
       <RegisterForm />
