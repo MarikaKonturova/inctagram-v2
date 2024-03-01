053bb813f4c19aae046ee77aaa45269bfa606e9c
Margarita P <rprivalko@gmail.com>
2024-03-01 18:12:17 +0300

    SCRUM-188 fix: profile localizations

--- src/features/account-management-form/model/index.ts
+++ src/features/account-management-form/model/index.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { type AxiosError } from 'axios'
 import { format } from 'date-fns'
+import { useTranslation } from 'next-i18next'
 import { SubscriptionsService } from 'shared/api'
 import { useSnackbar } from 'shared/hooks'
 import { type CostOfSubscriptionType, type SubscriptionType } from 'shared/types/subscriptions'
@@ -9,6 +10,7 @@ const formatDate = (date: string) => format(new Date(date), 'dd.MM.yyyy')
 
 export const useSubscriptions = (selected: CostOfSubscriptionType) => {
   const onOpen = useSnackbar(state => state.onOpen)
+  const { t } = useTranslation('profile')
 
   const { data: costOfSubscription, isLoading } = useQuery({
     onError: (error: Error) => {
@@ -67,12 +69,16 @@ export const useSubscriptions = (selected: CostOfSubscriptionType) => {
   const expireAt = endDateOfSubscription && formatDate(endDateOfSubscription)
   const nextPayment = dateOfPayment && formatDate(dateOfPayment)
 
+  const localizedCostOfSubscription = costOfSubscription?.data.data.map(el => {
+    return { ...el, description: `${el.amount}$ ${t('per')} ${t(`${el.typeDescription}`)}` }
+  })
+
   return {
     cancelAutoRenewal,
     expireAt,
     hasAutoRenewal: currentSubscription?.data.hasAutoRenewal,
     nextPayment,
     onStripeHandler,
-    subscriptionCosts: costOfSubscription?.data.data || [],
+    subscriptionCosts: localizedCostOfSubscription || [],
   }
 }
